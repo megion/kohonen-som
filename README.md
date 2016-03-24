20c40f3df47d4b5cd019bc18b1668dddd57f025d
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-03-24 19:20:35 +0300

    test

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ $ mvn package
 
 ## Use spark-submit to run your application
 $ YOUR_SPARK_HOME/bin/spark-submit --class "org.megion.spark.mllib.kohonen.som.ExampleSOM" --master local[2] target/kohonen-som-1.0.jar
+
+$ /home/ilya/frameworks/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class "org.megion.spark.mllib.kohonen.som.ExampleSOM" --master local[2] target/kohonen-som-1.0.jar
--- src/main/java/org/megion/spark/mllib/kohonen/som/ExampleSOM.java
+++ src/main/java/org/megion/spark/mllib/kohonen/som/ExampleSOM.java
@@ -8,7 +8,7 @@ import org.apache.spark.api.java.function.Function2;
 public class ExampleSOM {
 
     public static void main(String[] args) {
-        String logFile = "/home/ilya/eclipses/spark-1.6.0-bin-hadoop2.6/README.md"; // Should be some file on your system
+        String logFile = "/home/ilya/frameworks/spark-1.6.0-bin-hadoop2.6/README.md"; // Should be some file on your system
         SparkConf conf = new SparkConf().setAppName("Simple Application");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> logData = sc.textFile(logFile).cache();
@@ -23,6 +23,27 @@ public class ExampleSOM {
 
         System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
 
+
+//        class ColMedian {
+//            List<Double> medians;
+//            List<Long> counts;
+//        }
+        //
+
+//        class CalculateMedian implements Function<String, ColMedian> {
+//            public ColMedian call(String s) {
+//                ColMedian colMedians = new ColMedian();
+//                // инициализация colMedians нулями, если 20 колонок нам нужны то 20 нулей
+//                // разбор строки на колонки List<String> cols
+//                // если данные допустимы в колонке (cols[i]!='X' или cols[i]!='-1' тут нужно учесть
+//                // что для каждой колонки допустимые значения разные) то colMedians.medians[i]+=cols[i], colMedians.counts[i]++
+//                return colMedians;
+//            }
+//        }
+
+//        JavaRDD<Integer> lineLengths = lines.map(new CalculateMedian());
+//        ColMedian med = lineLengths.reduce(new ReduceMedian());
+
         class GetLength implements Function<String, Integer> {
             public Integer call(String s) {
                 return s.length();
@@ -35,9 +56,27 @@ public class ExampleSOM {
             }
         }
 
-        JavaRDD<String> lines = sc.textFile("/home/ilya/eclipses/spark-1.6.0-bin-hadoop2.6/README.md");
+        JavaRDD<String> lines = sc.textFile("/home/ilya/frameworks/spark-1.6.0-bin-hadoop2.6/README.md");
         JavaRDD<Integer> lineLengths = lines.map(new GetLength());
         int totalLength = lineLengths.reduce(new Sum());
         System.out.println("totalLength: " + totalLength);
+
+        // получение треугольной квадратной матрицы ковариации
+//        List<List<Double>> squareMatrix;
+//        int colSize = 20;
+//        while (dataReader->readNext(inRow)) { //цикл по RDD
+//            for (int i = 0; i < colSize; ++i) {
+//                if (inDataRow[i] имеет не допустимое значение) {
+//                    continue;
+//                }
+//                for (int j = i; j < colSize; ++j) {
+//                    if (inDataRow[j] допустимое значение) {
+//                        squareMatrix[i, j] = squareMatrix[i, j] +
+//                                (inDataRow[i] - colMedians[i]) *
+//                                        (inDataRow[j] - colMedians[j]);
+//                    }
+//                }
+//            }
+//        }
     }
 }
